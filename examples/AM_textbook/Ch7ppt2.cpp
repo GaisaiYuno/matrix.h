c4ff2cf3afbb0284d37b903763b070afabd96162
Steven Meng <52853137+GaisaiYuno@users.noreply.github.com>
2022-12-23 11:11:08 +0800

    Create Ch7ppt2.cpp

---
+++ examples/AM_textbook/Ch7ppt2.cpp
@@ -0,0 +1,11 @@
+#include "poly.h"
+#define Num poly
+#include "euclid.h"
+using namespace std;
+int main(){
+    Matrix P1=Matrix('R',vector<poly>{1,-2,1,1});
+    Matrix P2=Matrix('R',vector<poly>{1,1,2,-3});
+    Matrix N1=P1/length(Nvec(P1));
+    Matrix N2=P2/length(Nvec(P2));
+    cout<<N1+N2<<N1-N2<<endl;
+}
